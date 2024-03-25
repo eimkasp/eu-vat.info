327e9126981c34d9e65c7f8fe1927e8dac54cd3c
Eimantas <eim.kasp@gmail.com>
2024-03-25 07:59:33 +0200

    Country single updates

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,9 @@
     <div class="xl:grid xl:grid-cols-3 xl:gap-8">
       <div class="space-y-8">
         LOGO
-        <p class="text-sm leading-6 text-gray-600">Making the world a better place through constructing elegant hierarchies.</p>
+        <p class="text-sm leading-6 text-gray-600">
+          Our goal is to provide simple and easy way to navigate VAT rates in Europe.
+        </p>
         <div class="flex space-x-6">
           {{-- TODO: Add Socials Here --}}
         </div>
@@ -12,72 +14,27 @@
       <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
-            <h3 class="text-sm font-semibold leading-6 text-gray-900">Solutions</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Marketing</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Analytics</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Commerce</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Insights</a>
-              </li>
-            </ul>
+            
           </div>
           <div class="mt-10 md:mt-0">
-            <h3 class="text-sm font-semibold leading-6 text-gray-900">Support</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Pricing</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Documentation</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Guides</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">API Status</a>
-              </li>
-            </ul>
+            
           </div>
         </div>
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
-            <h3 class="text-sm font-semibold leading-6 text-gray-900">Company</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
-              </li>
-            </ul>
+           
           </div>
           <div class="mt-10 md:mt-0">
-            <h3 class="text-sm font-semibold leading-6 text-gray-900">Legal</h3>
+            <h3 class="text-sm font-semibold leading-6 text-gray-900">Tools</h3>
             <ul role="list" class="mt-6 space-y-4">
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Claim</a>
+                <a href="/vat-calculator" class="text-sm leading-6 text-gray-600 hover:text-gray-900">VAT Calculator</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Privacy</a>
+                <a href="/vat-validator" class="text-sm leading-6 text-gray-600 hover:text-gray-900">VAT Validator</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Terms</a>
+                <a href="/tools" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Resources and tools</a>
               </li>
             </ul>
           </div>
@@ -85,7 +42,7 @@
       </div>
     </div>
     <div class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-      <p class="text-xs leading-5 text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+      <p class="text-xs leading-5 text-gray-500">&copy; Data as of: 2024 March 24 EU-VAT.info</p>
     </div>
   </div>
 </footer>
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,11 +7,85 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
     <title>{{ $title ?? 'Page Title' }}</title>
+<style>
+.container {
+    max-width: 1140px;
+    margin: 0 auto;
+}
+
+h1 {
+    font-weight: 700;
+}
+
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-weight: 600 !important;
+    margin-top: 20px !important;
+}
+
+h2 {
+    font-size: 1.6rem !important;
+}
+
+h3 {
+    font-size: 1.4rem !important;
+}
+
+h4 {
+    font-size: 1.25rem !important;
+}
+
+h5 {
+    font-size: 1rem;
+}
+
+h6 {
+    font-size: 1rem;
+}
+
+p {
+    font-size: 1rem;
+    line-height: 1.5;
+    margin-bottom: 20px !important;
+}
+
+
+.country-content a {
+    color: blue !important;
+    text-decoration: underline !important;
+}
+
+.sticky-element, .sticky-element > div {
+    transition: all 0.3s ease;
+}
+
+.sticky-element.is-pinned {
+   
+}
+
+.sticky-element.is-pinned > div {
+     margin-top: 100px;
+}
+
+.sticky-element .country-sub-details {
+    transition: all 0.4s ease-in-out !important;
+    height: 20px;
+}
+
+.sticky-element.is-pinned .country-sub-details {
+    height: 0px;
+    opacity: 0;
+    overflow: hidden;
+}
+</style>
 </head>
 
 <body>
     <x-global-header></x-global-header>
-    
+
     <div class="bg-gray-50">
         {{ $slot }}
     </div>
