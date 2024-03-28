797f452bde45b217885e82b0a3d765b8cb7b8ab5
Eimantas <eim.kasp@gmail.com>
2024-03-28 16:54:01 +0200

    Add Adsense script + Refactoring

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,85 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-  @vite('resources/css/app.css')
+    @vite('resources/css/app.css')
 
     <title>@yield('title', 'EU VAT Info')</title>
     <meta name="description" content="@yield('meta_description', 'Our goal is to provide you with the most up-to-date information on VAT in the European Union. And help you guide VAT compliance landscape')">
-    <style>
-        .container {
-            max-width: 1140px;
-            margin: 0 auto;
-        }
-
-        h1 {
-            font-weight: 700 !important;
-            font-size: 1.875rem !important;
-        }
-
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-weight: 600 !important;
-            margin-top: 20px !important;
-        }
-
-        h2 {
-            font-size: 1.6rem !important;
-        }
-
-        h3 {
-            font-size: 1.4rem !important;
-        }
-
-        h4 {
-            font-size: 1.25rem !important;
-        }
-
-        h5 {
-            font-size: 1rem;
-        }
-
-        h6 {
-            font-size: 1rem;
-        }
-
-        p {
-            font-size: 1rem;
-            line-height: 1.5;
-            margin-bottom: 20px !important;
-        }
-
-
-        .country-content a {
-            color: blue !important;
-            text-decoration: underline !important;
-        }
-
-        .sticky-element,
-        .sticky-element>div {
-            transition: all 0.3s ease;
-        }
-
-        .sticky-element.is-pinned {}
-
-        .sticky-element.is-pinned>div {
-            margin-top: 100px;
-        }
-
-        .sticky-element .country-sub-details {
-            transition: all 0.4s ease-in-out !important;
-            height: 20px;
-        }
-
-        .sticky-element.is-pinned .country-sub-details {
-            height: 0px;
-            opacity: 0;
-            overflow: hidden;
-        }
-    </style>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3925599852702124"
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
