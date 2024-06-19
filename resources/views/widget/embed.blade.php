a91389c2851b233c57aff739029546149107899c
Eimantas <eim.kasp@gmail.com>
2024-06-19 22:34:20 +0300

    Update embed.blade.php

--- resources/views/widget/embed.blade.php
+++ resources/views/widget/embed.blade.php
@@ -1,24 +1,27 @@
 @extends('components.layouts.app')
 @section('title', 'Embed VAT Calculator to your website - EU VAT Info')
-@section('meta_description', 'Embed this VAT Calculator widget on your website to show the current VAT rates for all EU countries. Customization options available.')
+@section('meta_description', 'Embed this VAT Calculator widget on your website to show the current VAT rates for all EU
+    countries. Customization options available.')
 @section('content')
     <div class="container mx-auto !py-12">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-9">
             <div>
                 <h1 class="text-4xl font-bold mb-4">Embed EU VAT Widget
-                @isset($selectedCountry->name)
-                    for {{ $selectedCountry->name }}
-                @endisset
+                    @isset($selectedCountry->name)
+                        for {{ $selectedCountry->name }}
+                    @endisset
                 </h1>
                 <p class="mb-4">Embed this widget on your website to show the current VAT rates for all EU countries.</p>
                 <div class="bg-white p-4 rounded-lg shadow-md">
                     <h2 class="text-xl font-bold mb-4">Embed code</h2>
-                    <pre id="embed-code" class="bg-gray-100 p-4 rounded-lg w-full break-all whitespace-pre-wrap">&lt;iframe src="{{ route('widget.iframe') }}@isset($selectedCountry->slug)/{{ $selectedCountry->slug }}@endisset" style="border:none;outline: none;background: transparent;box-shadow: none;" width="100%" height="400px" frameborder="0"&gt;&lt;/iframe&gt;
+                    <pre id="embed-code" class="bg-gray-100 p-4 rounded-lg w-full break-all whitespace-pre-wrap">&lt;iframe src="{{ route('widget.iframe') }}@isset($selectedCountry->slug)
+/{{ $selectedCountry->slug }}
+@endisset" style="border:none;outline: none;background: transparent;box-shadow: none;" width="100%" height="400px" frameborder="0"&gt;&lt;/iframe&gt;
         </pre>
                     <div class="flex items-center justify-between mt-6">
-                        <div class="btn btn-primary plausible-event-name=CopyCode" x-data="{}" @click="navigator.clipboard.writeText(document.getElementById('embed-code').innerText)"
-
-                        >Copy code
+                        <div class="btn btn-primary plausible-event-name=CopyCode" x-data="{}"
+                            @click="navigator.clipboard.writeText(document.getElementById('embed-code').innerText)">Copy
+                            code
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                 stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
@@ -67,7 +70,9 @@
                     </div>
                 </div>
             </div>
-
-
+            <div class="sm:col-span-2 mt-9">
+                <x-country-calculator-list>
+                </x-country-calculator-list>
+            </div>
         </div>
     @endsection
